1e3f5994d47843dd43c3f098ad59ee8bd7028c75
don-kakashi <idriss0said0alaoui@gmail.com>
2017-10-29 14:40:38 +0000

    Controller and View are able to delete or add a member of a team

--- team/urls.py
+++ team/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'team'
 urlpatterns =  [
     url(r'^$', views.index, name='index'),
-    url(r'^create/$', views.create, name='create'),
+    url(r'^add/$', views.add, name='add'),
+    url(r'^delete/$', views.delete, name='delete'),
 ]
--- team/views.py
+++ team/views.py
@@ -3,19 +3,29 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'team_list.html', {})
 
-def create(request):
+def add(request):
     if not request.user.is_authenticated():
         return HttpResponseRedirect('/profiles/login')
-    #if request.method == 'POST':
-        # project_title = request.POST['project_title']
-        # estimate_hours = request.POST['estimate_hours']
-        # team_names = request.POST.getlist('team_names')
+    if request.method == 'POST':
+        new_member = request.POST['new_member']
 
         # TO BE REMOVED, DATA to be saved in DB
-        # print(project_title)
-        # print(estimate_hours)
-        # print(team_names)
+        print(new_member)
+
+        return render(request, 'team_list.html', {})
 
         # choose where to redirect the user after successul data addition
-        #return HttpResponseRedirect('/dashboard')
-    return render(request, 'team_list.html', {})
+        # render(request, 'team_list.html', {})
+def delete(request):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect('/profiles/login')
+    if request.method == 'POST':
+        member_id = request.POST['member_id']
+
+        # TO BE REMOVED, DATA to be saved in DB
+        print(member_id)
+
+        return render(request, 'team_list.html', {})
+
+        # choose where to redirect the user after successul data addition
+        # render(request, 'team_list.html', {})
