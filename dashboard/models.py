30ff0cf1a9b53191eb4859547f43d5d6f708299e
Najwa Laabid <najwalaabid@gmail.com>
2017-10-29 22:02:05 +0000

    Model for dashboard - project

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+	project_ID = models.AutoField(primary_key=True)
+	project_code = model.CharField(max_length=200, unique=True)
+	project_title = model.CharField(max_length=200)
+	project_status = (
+        (ON_GOING, 'On going'),
+        (HOLD, 'Hold'),
+        (AT_RISK, 'At risk'),
+    )
+	project_phase = (
+		(CASE_STUDY, 'Case Study'),
+        (CONCEPTUAL, 'Conceptual'),
+        (BASIC_ENGINEERING, 'Basic Engineering'),
+        (DETAILED_ENGINEERING, 'Detailed Engineering'),
+	)
+	jesa_role = (
+		(EPC, 'EPC'),
+		(PMC,'PMC'),
+		(EPCM, 'EPCM'),
+	)
+
+	estimated_hours = models.IntegerField()
+
+	actual_hours = models.IntegerField()
+
+	start_date = models.DateTimeField()
+
+	end_date = models.DateTimeField()
+
+	
+
+
