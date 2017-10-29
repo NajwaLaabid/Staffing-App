bab95d7d3b18c0d99042766377d20bd2c506c28c
don-kakashi <idriss0said0alaoui@gmail.com>
2017-10-29 15:41:16 +0000

    view details of a project

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,4 +6,6 @@ app_name = 'dashboard'
 urlpatterns =  [
     url(r'^$', views.index, name='index'),
     url(r'^create/$', views.create, name='create'),
+    url(r'^close/$', views.close, name='close'),
+    url(r'^view/(?P<project_id>\d+)/$', views.view, name='view'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html', {})
 
+def view(request, project_id):
+    return render(request, 'details.html', {})
+
 def create(request):
     if not request.user.is_authenticated():
         return HttpResponseRedirect('/profiles/login')
@@ -15,7 +18,21 @@ def create(request):
         print(project_title)
         print(estimate_hours)
         print(team_names)
-        
+
         # choose where to redirect the user after successul data addition
         #return HttpResponseRedirect('/dashboard')
     return render(request, 'create.html', {})
+
+def close(request):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect('/profiles/login')
+    if request.method == 'POST':
+        project_id = request.POST['project_id']
+
+        # TO BE REMOVED, DATA to be saved in DB
+        print(project_id)
+
+        return render(request, 'index.html', {})
+
+        # choose where to redirect the user after successul data addition
+        # render(request, 'team_list.html', {})
