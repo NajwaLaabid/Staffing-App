4eb6fc75ecfc52cc02a9d32a266fafefed70555c
Najwa Laabid <najwalaabid@gmail.com>
2017-10-30 23:21:41 +0000

    added employee cards

--- team/views.py
+++ team/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.template.response import TemplateResponse
 from .models import Employee
 
 def index(request):
-    return render(request, 'team_list.html', {})
+    employees = Employee.objects.all()
+    return TemplateResponse(request, 'teamIndex.html', {'employees': employees,})
+
+def getEmployeeStatus(request):
+    statuses = Employee.objects.all()
+    return TemplateResponse(request, 'teamIndex.html', {'employees': employees,})
 
 def addEmployee(request):
     if not request.user.is_authenticated():
